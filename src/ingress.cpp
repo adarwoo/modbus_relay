a0f2b782c602befcc06437b5f0f4d49ad690d3f6
guillaume.arreckx <software@arreckx.com>
2025-05-17 01:18:17 +0200

    Ready to be tested

--- src/ingress.cpp
+++ src/ingress.cpp
@@ -1,15 +1,20 @@
 #include <cmath>
 #include <chrono>
+
+#include <asx/hw_timer.hpp>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #include "fft.hpp"
 #include "ingress.hpp"
+
 
 namespace ingress {
    static constexpr auto CENTER_FREQ = 50; // Center frequency for the FFT
    static constexpr auto FFT_SIZE = 64; // Size of the FFT
    static constexpr auto ADC_SAMPLES_RATE = 320; // Sample rate in Hz
-   static constexpr auto SAMPLE_PERIOD = std::chrono::duration<double>(1.0 / ADC_SAMPLES_RATE);
+   static constexpr auto SAMPLE_PERIOD = asx::chrono::cpu_tick_t(
+      static_cast<long long>(F_CPU / ADC_SAMPLES_RATE));
 
    static constexpr auto ADC_CLK_PRESCALE = 2;
    static constexpr auto ADC_CLK_PER = F_CPU / ADC_CLK_PRESCALE;
@@ -18,10 +23,7 @@ namespace ingress {
    static constexpr auto ADC_SAMPLE_DURATION = 2; // We have a low impedance input with input capacitange of 8pF, we can sample fast.
 
    // Create the FFT instance
-   using fft_t = asx::fft::FFT<FFT_SIZE, ADC_SAMPLES>;
-
-   // Create an instance of the FFT class
-   auto fft = fft_t {};
+   using fft_t = asx::fft::FFT<FFT_SIZE, ADC_SAMPLES_RATE>;
 
    void handle_fft_result(uint16_t result) {
       // Handle the FFT result
@@ -31,7 +33,7 @@ namespace ingress {
    }
 
    void send_to_fft(int16_t adc_sample) {
-      fft::compute(adc_sample);
+      fft_t::next(adc_sample);
    }
 
    auto react_on_adc_sample_ready = asx::reactor::bind(send_to_fft);
@@ -40,12 +42,12 @@ namespace ingress {
       // Initialize the ingress system
 
       // Initialize the FFT instance for detecting 50Hz
-      fft.init<CENTER_FREQ>(asx::reactor::bind(handle_fft_result));
+      fft_t::init<CENTER_FREQ>(asx::reactor::bind(handle_fft_result));
 
       // Initialise the ADC for the ingress system
 
       // Enable (won't start yet - needs a command)
-      ADC0.CTRLA = ADC_LOWLAT_gc | ADC_ENABLE;
+      ADC0.CTRLA = ADC_LOWLAT_bm | ADC_ENABLE_bm;
 
       // Select the ADC clock
       ADC0.CTRLB = ADC_PRESC_DIV2_gc;
@@ -57,32 +59,35 @@ namespace ingress {
       ADC0.CTRLE = ADC_SAMPLE_DURATION;
 
       // Accumulate 512 samples to gain 4 bits of resolution
-      ADC0.CTRLF = ADC_SAMPNUM_ACC512;
+      ADC0.CTRLF = ADC_SAMPNUM_ACC512_gc;
 
       // No PGA
-      ADC0.PGACTRL = ADC_PGA_DISABLE;
+      ADC0.PGACTRL = 0;
 
       // Select the ADC channel
       ADC0.MUXPOS = ADC_MUXPOS_2_bm;
       ADC0.MUXNEG = ADC_MUXNEG_2_bm;
-      ADC0.PGACTRL = 0; // No PGA
 
       // Ready the command using a event to start the ADC
-      ADC0.COMMAND = ADC_COMMAND_DIFF_bm | ADC_COMMAND_NODE_BURST_SCALING_gc | ADC_COMMAND_EVENT_TRIGGER_bm;
+      ADC0.COMMAND = ADC_DIFF_bm | ADC_MODE_BURST_SCALING_gc | ADC_START_EVENT_TRIGGER_gc;
 
       // Use timer B0 to trigger the ADC through the event system
       using timer = asx::hw_timer::TimerB<0>;
+      using namespace std::chrono;
       timer::set_compare( SAMPLE_PERIOD );
 
       // Hook the event system to trigger the ADC
       EVSYS.CHANNEL0 = EVSYS_CHANNEL0_TCB0_CAPT_gc;    // Rx/Tx activity
-      EVSYS.USERADC0 = EVSYS_USER_CHANNEL0_gc;
+      EVSYS.USERADC0START = EVSYS_USER_CHANNEL0_gc;
 
       // Enable the interrupt
-      ADC0.INTCTRL |= ADC_RESREADY;
+      ADC0.INTCTRL |= ADC_RESRDY_bm;
    }
 
-   ISR(ADC0_vect) {
-      react_on_adc_sample_ready(ADC0.RESULT16);
+   /**
+    * @brief ADC interrupt handler
+    */
+   ISR(ADC0_RESRDY_vect) {
+      react_on_adc_sample_ready(static_cast<uint16_t>(ADC0.RESULT));
    }
 } // namespace ingress
